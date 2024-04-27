1d88bc349083fb176a66c24fe7aa57eb2e62d1b9
trabelsi.ahmed.1@esprit.tn <trabelsi.ahmed.1@esprit.tn>
2024-04-27 20:57:29 +0200

    correction front docker55555555858555585888

--- HR_Managment_application_Backend-/src/main.ts
+++ HR_Managment_application_Backend-/src/main.ts
@@ -9,12 +9,10 @@ async function bootstrap() {
   await app.listen(process.env.SERVER_PORT) 
   
   app.enableCors( {
-    origin: ['http://localhost:4200', 'http://192.168.33.10:4200'],   // Adjust according to your front-end URL
+    origin: [ 'http://192.168.33.10:4200'],   // Adjust according to your front-end URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
+  
   });
   app.use(cookieParser());
   
